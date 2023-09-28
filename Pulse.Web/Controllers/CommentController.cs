1ae3d5ecf51f5eb829b375b0dffff8cd48855600
richard <richard.zsupos@gmail.com>
2023-09-28 17:00:53 +0200

    Add comment controller

---
+++ Pulse.Web/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pulse.Core.DTOs;
+using Pulse.Core.Feedback;
+using Pulse.Core.Interfaces.Services;
+
+namespace Pulse.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommentController
+{
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> AddComment(CreateCommentDto comment)
+    {
+        Response response = new();
+
+        try
+        {
+            response = await _commentService.AddComment(comment);
+            return new OkObjectResult(response);
+        }
+        catch (Exception ex)
+        {
+            response.Messages.Add(ex.Message);
+            return new BadRequestObjectResult(response);
+        }
+    }
+}
